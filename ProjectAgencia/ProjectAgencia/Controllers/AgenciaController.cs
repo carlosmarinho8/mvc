c2083dec4af2f9b3b2cb856d13efec5c93a2ecd9
carlosmarinho8 <rickmarinho65@gmail.com>
2021-12-18 15:29:45 -0300

    first

---
+++ ProjectAgencia/ProjectAgencia/Controllers/AgenciaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAgencia.Controllers
+{
+    public class AgenciaController : Controller
+    {
+        // GET: Agencia
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult destinos()
+        {
+            return View();
+        }
+        public ActionResult promocoes()
+        {
+            return View();
+        }
+        public ActionResult contato()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectAgencia/ProjectAgencia/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAgencia.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Sexo { get; set; }
+        public int CPF { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
